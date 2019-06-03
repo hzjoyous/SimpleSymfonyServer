574615c54bfb8f8a68d6fe5d7108f59529dbeee9
HZJ <1054919923@qq.com>
2019-06-03 16:16:50 +0800

    update all

---
+++ deploy.php
@@ -0,0 +1,55 @@
+<?php
+namespace Deployer;
+
+require 'recipe/symfony4.php';
+
+// Project name
+set('application', 'api.doufugang.nonodi.com');
+
+// Project repository
+set('repository', 'https://github.com/doufugang/doufugangserver.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', false);
+
+// Shared files/dirs between deploys 
+set('shared_files', [
+    '.env.local'
+]);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+// Hosts
+
+// 开发环境
+
+host(
+    '111.231.202.11'
+    )
+    ->stage('prod')
+    // ->set('branch', 'production')
+    ->user('www')
+    ->set('deploy_path', '/home/www/{{application}}');
+
+
+task('opcache_clear', function() {
+    // run('curl http://opcr.xiaozhu.com/index.php');
+});
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+after('deploy:symlink', 'opcache_clear');
+// Migrate database before symlink new release.
+//database:migrate 暂不需要
+//before('deploy:symlink', 'database:migrate');
+
+ 
