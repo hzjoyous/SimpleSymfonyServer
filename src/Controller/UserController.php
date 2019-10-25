c673804757d4f86eb0a4c12487297ff720933f95
HZJ <1054919923@qq.com>
2019-10-26 02:32:57 +0800

    'update_all'

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Session;
+use App\Entity\User;
+use App\Exception\BusinessException;
+use App\Repository\SessionRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Lcobucci\JWT\Parser as JWTParer;
+use Lcobucci\JWT\Builder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
@@ -14,17 +23,135 @@ class UserController extends AbstractController
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
+            'path'    => 'src/Controller/UserController.php',
         ]);
     }
 
-    public function login()
+    /**
+     * @Route("/login", name="登陆")
+     * @param RequestStack $requestStack
+     * @param EntityManagerInterface $entityManager
+     * @param UserRepository $fitnessStatisticsRepository
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function login(RequestStack $requestStack, EntityManagerInterface $entityManager, UserRepository $userRepository)
     {
+        $requestContent = $requestStack->getMasterRequest()->getContent();
+        $requestArr     = json_decode($requestContent, true);
+        $username       = $requestArr['username'] ?? '';
+        $password       = $requestArr['password'] ?? '';
 
+        $user = $userRepository->findOneBy([
+            'username' => $username,
+            'password' => $password,
+        ]);
+        if ($user === null) {
+            throw new BusinessException('登陆信息输入有误');
+        }
+        $session = new Session();
+        $session->setUserId($user->getId());
+        $session->setExp(time() + 3600);
+        $session->setOrigin('api');
+        $entityManager->persist($session);
+        $entityManager->flush();
+
+        $time  = (new \DateTime())->getTimestamp();
+        $token = (new Builder())
+            ->issuedBy('https://nonodi.com')
+            ->issuedAt($time)// Configures the time that the token was issue (iat claim)
+            ->canOnlyBeUsedAfter($time + 0)// Configures the time that the token can be used (nbf claim)
+            ->expiresAt($time + 60)// Configures the expiration time of the token (exp claim)
+            ->withClaim('username', $user->getUsername())
+            ->withClaim('sessionId', $session->getId())
+            ->getToken();
+
+        return $this->json([
+            'status'  => 0,
+            'message' => 'success',
+            'value'   => [
+                'token'    => (string)$token,
+                'userInfo' => [
+                    'username' => $user->getUsername()
+                ]
+            ],
+        ]);
     }
 
-    public function reg()
+    /**
+     * @Route("/register", name="注册")
+     * @param RequestStack $requestStack
+     * @param EntityManagerInterface $entityManager
+     * @param UserRepository $fitnessStatisticsRepository
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function register(RequestStack $requestStack, EntityManagerInterface $entityManager, UserRepository $userRepository)
     {
+        $requestContent = $requestStack->getMasterRequest()->getContent();
+        $requestArr     = json_decode($requestContent, true);
+        $username       = $requestArr['username'] ?? '';
+        $password       = $requestArr['password'] ?? '';
 
+        if (!($username && $password)) {
+            throw new BusinessException("输入不合法");
+        }
+
+        $user = new User();
+        $user->setUsername($username);
+        $user->setPassword($password);
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->json([
+            'status'  => 0,
+            'message' => 'success',
+            'value'   => 1,
+        ]);
+    }
+
+    /**
+     * @Route("/jwtCheck", name="注册")
+     * @param RequestStack $requestStack
+     * @param EntityManagerInterface $entityManager
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function jwtCheck(RequestStack $requestStack, EntityManagerInterface $entityManager)
+    {
+        $requestContent = $requestStack->getMasterRequest()->getContent();
+        $requestArr     = json_decode($requestContent, true);
+        $tokenContent   = $requestArr['token'];
+        $token          = (new JWTParer())->parse((string)$tokenContent);
+        if ($token->isExpired()) {
+            throw new BusinessException('认证已过期');
+        }
+        return $this->json([
+            'status'  => 0,
+            'message' => 'success',
+            'value'   => 1
+        ]);
+    }
+
+    /**
+     * @Route("/sessionCheck", name="session验证")
+     * @param RequestStack $requestStack
+     * @param EntityManagerInterface $entityManager
+     * @param SessionRepository $sessionRepository
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function sessionCheck(RequestStack $requestStack, EntityManagerInterface $entityManager, SessionRepository $sessionRepository)
+    {
+        $requestContent = $requestStack->getMasterRequest()->getContent();
+        $requestArr     = json_decode($requestContent, true);
+        $tokenContent   = $requestArr['token'];
+        $token          = (new JWTParer())->parse((string)$tokenContent);
+        $session        = $sessionRepository->findOneBy(['id' => $token->getClaim('sessionId')]);
+        dd($session);
+        if (!$session && $session->getStatus() !== 1 && $session->getExp() < time()) {
+            throw new BusinessException('会话过期');
+        }
+        return $this->json([
+            'status'  => 0,
+            'message' => 'success',
+            'value'   => 1
+        ]);
     }
 }
---
+++ src/Entity/Session.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SessionRepository")
+ */
+class Session
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=4096)
+     */
+    private $data = '{}';
+
+    private $dataObjGetFromData = false;
+
+    /**
+     * @var array
+     */
+    private $dataObj = [];
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $exp;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status = '1';
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $userId;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $origin;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    private function getData(): ?string
+    {
+        return $this->data;
+    }
+
+    private function setData(string $data): self
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    public function getExp(): ?string
+    {
+        return $this->exp;
+    }
+
+    public function setExp(string $exp): self
+    {
+        $this->exp = $exp;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): self
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    public function getOrigin(): ?string
+    {
+        return $this->origin;
+    }
+
+    public function setOrigin(string $origin): self
+    {
+        $this->origin = $origin;
+
+        return $this;
+    }
+
+    public function setDataObj($key, string $value = ''): self
+    {
+        $this->dataObj[$key] = $value;
+        $this->data          = json_encode($this->dataObj);
+        return $this;
+    }
+
+    public function getDataObjValue($key): string
+    {
+        if ($this->dataObjGetFromData === 0) {
+            $this->dataObjGetFromData = 1;
+            $this->dataObj            = json_decode($this->data);
+            if ($this->dataObj) {
+                $this->dataObj = [];
+            }
+        }
+        return $this->dataObj[$key] ?? '';
+    }
+}
