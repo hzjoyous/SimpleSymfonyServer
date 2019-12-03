3ae4dad96fb4e6cb3b8603b20b4f030ddfa35d90
Your Name <you@example.com>
2019-12-03 10:19:52 +0800

    update symfony-4.4

--- src/Repository/SessionRepository.php
+++ src/Repository/SessionRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Session;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Session|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class SessionRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Session::class);
     }
